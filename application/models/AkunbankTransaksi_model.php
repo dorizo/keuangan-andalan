b46efbeef6d3a17eeda40c2ba581c480ea090e0a
doris hermawan <dorizo89@gmail.com>
2022-12-19 18:42:50 +0700

    data

--- application/models/AkunbankTransaksi_model.php
+++ application/models/AkunbankTransaksi_model.php
@@ -33,9 +33,32 @@ class akunbankTransaksi_model extends CI_Model {
             $this->db->delete("akunbank_transaksi");
         }
         public function submitadd(){
+            
+            $this->db->trans_begin();
             $p =  $this->input->post();
+            $julahtransaksi = str_replace(",", "",$this->input->post("transaksiJumlah"));
             $p["transaksiJumlah"] =  str_replace(",", "",$this->input->post("transaksiJumlah"));
             $this->db->insert("akunbank_transaksi" , $p);
+            $a = $this->db->query('Select saldo_sekarang from akunBank where akunBankCode='.$this->input->post("akunBankCode"))->row();
+           if($this->input->post("statusTransaksi") == "CR"){
+            // credit saldo mengurangi dan input ke sini
+            $saldo = $a->saldo_sekarang - str_replace(",", "",$this->input->post("transaksiJumlah"));
+            $this->db->query("UPDATE `akunbank` SET saldo_sekarang=$saldo where akunBankCode=".$this->input->post("akunBankCode"));
+            $this->db->query("INSERT INTO `akunbank_debit_kredit`( `akunbankCode`, `debit`, `credit`, `saldo`)VALUES (".$this->input->post("akunBankCode").",0,".$julahtransaksi.",".$saldo.")");
+            }else{
+                $saldo = $a->saldo_sekarang + str_replace(",", "",$this->input->post("transaksiJumlah"));
+                $this->db->query("UPDATE `akunbank` SET saldo_sekarang=$saldo where akunBankCode=".$this->input->post("akunBankCode"));
+                $this->db->query("INSERT INTO `akunbank_debit_kredit`( `akunbankCode`, `debit`, `credit`, `saldo`)VALUES (".$this->input->post("akunBankCode").",".$julahtransaksi.",0,".$saldo.")");
+                    
+            }
+            if ($this->db->trans_status() === FALSE)
+            {
+                    $this->db->trans_rollback();
+            }
+            else
+            {
+                    $this->db->trans_commit();
+            }
         }
 
        
