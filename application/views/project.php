173d40f893befa6f94840bd8e1b7ba04ba1f27da
doris hermawan <dorizo89@gmail.com>
2023-01-20 10:05:23 +0700

    ss

--- application/views/project.php
+++ application/views/project.php
@@ -49,7 +49,7 @@
                           <?=projectmenu("CPRO",base_url("pengajuan/setting/".$value["project_id"]) , "fa-money-bill" , "Pengajuan")?>
                           <?=projectmenu("KEUW",base_url("project/generate/".$value["project_id"]) , "fa-money-bill" , "Generate COST CENTER")?>
                           <?=projectmenu("UPRO",base_url("project/edit/".$value["project_id"]) , "fa-money-bill" , "Edit")?>
-                          <?=projectmenu("KEUW",base_url("project/setting/".$value["project_id"]) , "fa-money-bill" , "Setting Project")?>
+                          <?=projectmenu("SETPRO",base_url("project/setting/".$value["project_id"]) , "fa-money-bill" , "Setting Project")?>
                           <?=projectmenu("KEUW",base_url("transaksi/setting/".$value["project_id"]) , "fa-money-bill" , "Pembayaran Vendor")?>
                           <?=projectmenu("KEUW",base_url("project/done/".$value["project_id"]) , "fa-money-bill" , "Penyelesaian Project")?>
                           <!-- <a class="dropdown-item" href="<?=base_url("project/setting/".$value["project_id"])?>"><i class="fas fa-money-bill"></i> </a>
