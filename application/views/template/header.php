361355c3e538d4b3c8e3fc22c7477be2a67fcd63
doris hermawan <dorizo89@gmail.com>
2023-01-20 10:21:46 +0700

    ss

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -263,7 +263,7 @@
             </a>
             <ul class="nav nav-treeview">
             <?=projectmenu("CR",base_url("Master/role") , "far fa-circle" , "ROLE")?>
-            <?=projectmenu("SUPE",base_url("Master/user") , "far fa-circle" , "USER")?>
+            <?=projectmenu("CU",base_url("Master/user") , "far fa-circle" , "USER")?>
               <!-- <li class="nav-item">
                 <a href="<?=base_url("/Master/role")?>" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
