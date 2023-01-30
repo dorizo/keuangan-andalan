bbed280adaf3cdf2613a64b53f5344b173ab6c03
doris hermawan <dorizo89@gmail.com>
2023-01-30 18:08:39 +0700

    ss

--- application/models/Report_model.php
+++ application/models/Report_model.php
@@ -6,7 +6,8 @@ class Report_model extends CI_Model {
                 $this->load->database();
         }
         public function view(){
-           return $this->db->query("SELECT count(*) as Count , sum(nilai_project) as nilai ,project_status FROM project GROUP BY project_status")->result_array();
+           return $this->db->query("SELECT count(*) as Count , sum(nilai_project) as nilai ,project_status FROM project JOIN job
+           ON job.job_name=project.project_status GROUP BY project_status ORDER BY job_id ASC")->result_array();
         }
     }
     ?>
