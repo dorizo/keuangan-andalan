e79ce4302d46937b2fd5e4e8627d101115870267
doris hermawan <dorizo89@gmail.com>
2023-02-10 20:54:55 +0700

    ss

---
+++ js/keuangan.js
@@ -0,0 +1,21 @@
+$(function () {
+    $("#example1").DataTable({
+      "responsive": true,
+      "pageLength": 100, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    $('#example2').DataTable({
+      "paging": true,
+      "lengthChange": false,
+      "searching": false,
+      "ordering": true,
+      "info": true,
+      "autoWidth": false,
+      "responsive": true,
+      "aoColumns": [
+        null,
+        null,
+        { "sType": "formatted-num" }
+    ]
+    });
+  });
