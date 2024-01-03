8fffcdd0fccc58edfac7da316e72ca5b0cd7fd53
doris hermawan <dorizo89@gmail.com>
2024-01-03 09:07:37 +0700

    sss

--- application/views/projectpart/addproject.php
+++ application/views/projectpart/addproject.php
@@ -23,8 +23,23 @@
                     
                   </div>
                   <div class="form-group">
+                          <label>witel</label>
+                          <select name="witel_id" class="custom-select">
+                              <option value="Pilih Vendor">Pilih witel</option>
+                              <?php
+                              foreach ($witelresult as $key => $value) {
+                                  # code...
+                                  $noted = $value['witel_id'] == $dataresult->witel_id ? ' selected="selected"' : '';
+                                  echo "<option value=\"".$value['witel_id']."\" ".$noted.">".$value['witel_name']."</option>";
+                              }
+                              ?>
+                      
+                          </select>
+                          
+                        </div>
+                  <div class="form-group">
                     <label for="exampleInputEmail1">Project Code</label>
-                    <input type="text" name="project_code" class="form-control">
+                    <input type="text" name="project_code" class="form-control" disabled >
                   </div>
                   <div class="form-group">
                     <label for="exampleInputEmail1">Project Name</label>
