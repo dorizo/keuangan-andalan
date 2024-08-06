fb461b902d29c5bf2f6904c3ef725cca80961c61
doris hermawan <dorizo89@gmail.com>
2024-08-06 09:34:59 +0700

    sss

--- application/views/template/footer.php
+++ application/views/template/footer.php
@@ -64,6 +64,7 @@ if(!empty($pluginjs)){
   $(function () {
     $("#example").DataTable({
       "responsive": true, "lengthChange": false, "autoWidth": false,
+      "bDestroy": true,
       "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
     }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
     $('#example2').DataTable({
@@ -74,6 +75,7 @@ if(!empty($pluginjs)){
       "info": true,
       "autoWidth": false,
       "responsive": true,
+      "bDestroy": true,
     });
   });
 </script>
