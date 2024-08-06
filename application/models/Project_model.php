3eb5caab1269617cc0c56abcc55a70fa24fb29e3
doris hermawan <dorizo89@gmail.com>
2024-08-06 10:38:24 +0700

    sss

--- application/models/Project_model.php
+++ application/models/Project_model.php
@@ -13,6 +13,7 @@ class Project_model extends CI_Model {
         $this->db->select("* ,(select witel_name from witel where witel_id=project.witel_id) as witel , (select vendorID from vendor where vendorCode=project.vendorCode) as vendor ,(select COALESCE(sum(a.transaksiJumlah),0) from akunbank_transaksi a where a.project_id=project.project_id ) as paymentvendor , (select  COALESCE(sum(hitungbunga( b.transaksiJumlah, b.transaksiDate , IF(a.project_paid IS NULL,NOW(),a.project_paid) )),0)  as x from project a JOIN  akunbank_transaksi b ON b.project_id=a.project_id where a.project_id=project.project_id) as totalbungaseluruh , (select COALESCE(sum(a.nilaibiaya), 0) from biayalaindetail a where a.project_id=project.project_id ) as pembayaranAPI");
         $this->db->join("project_cat" , "project.cat_id=project_cat.cat_id");
         $this->db->order_by("project_id" , "DESC");
+        $this->db->limit(200);
         $db = $this->db->get("project");
         return $db->result_array();
         }
