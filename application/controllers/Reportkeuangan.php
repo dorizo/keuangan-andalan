ef4f7510000471086de2f23909b30537bbfc1b38
doris hermawan <dorizo89@gmail.com>
2024-08-16 09:27:27 +0700

    sss

--- application/controllers/Reportkeuangan.php
+++ application/controllers/Reportkeuangan.php
@@ -43,5 +43,75 @@ class ReportKeuangan extends CI_Controller {
 		$this->load->view('template/footer');
     }
 
+    public function export(){
+        $date =$this->input->get("mulai");
+        $result =[];
+        $s = 1;
+        while (strtotime($date) <= strtotime($this->input->get("selesai"))) {
+           $transaksiproject = $this->db->query("select   DATE_FORMAT(a.transaksiDate, '%Y-%m-%d') as transaksiDate, MONTH(a.transaksiDate) as bulan , b.project_code ,d.AkunAkutansiName , d.AkunAkutansiCodeName , 'tidak ada' as akun, 'tidak ada' as kategori , witel_name , region_name , cat_name , a.transaksiNote, CASE WHEN a.statusTransaksi='DB' THEN a.transaksiJumlah ELSE NULL END AS debit , CASE WHEN a.statusTransaksi='CR' THEN a.transaksiJumlah ELSE NULL END AS kredit , vendorName from akunbank_transaksi a JOIN project b ON b.project_id=a.project_id  JOIN akunbank c ON a.akunBankCode=c.akunBankCode JOIN akunakutansi d ON d.AkunAkuntansiCode=a.AkunAkuntansiCode JOIN project e ON e.project_id=a.project_id JOIN witel f ON f.witel_id=e.witel_id JOIN region g ON g.region_id=f.region_id JOIN project_cat h ON e.cat_id=h.cat_id JOIN vendor i ON i.vendorCode=e.vendorCode where DATE_FORMAT(a.transaksiDate, '%Y-%m-%d') ='$date'")->result_array();
+            foreach ($transaksiproject as $key => $value) {
+                $row = array();
+                # code...
+                $row[]=$value["transaksiDate"];
+                $row[]= date("M",strtotime($value["transaksiDate"]));
+                $row[]=$value["project_code"];
+                $row[]=$value["AkunAkutansiName"];
+                $row[]=$value["AkunAkutansiCodeName"];
+                $row[]=$value["akun"];
+                $row[]=$value["kategori"];
+                $row[]=$value["witel_name"];
+                $row[]=null;
+                $row[]=$value["region_name"];
+                $row[]=$value["cat_name"];
+                $row[]=$value["transaksiNote"];
+                $row[]=null;
+                $row[]=$value["kredit"];
+                $row[]=null;
+                $row[]=null;
+                $row[]=$value["vendorName"];
+                $result[] = $row;
+                $s++;
+            }
+            // echo "select * from oprasional a where DATE_FORMAT(a.tanggal, '%Y-%m-%d') ='$date'";
+            
+            $this->db->join("akutansi_akun b" , "b.kode_akun=a.kode_akun");
+            $this->db->join("witelho c" , "c.witelhoID=a.witel_id");
+            $this->db->join("sto d" , "d.stoCode=a.stoCode");
+            $this->db->join("pekerjaan f" , "f.pekerjaanCode=a.pekerjaanCode");
+            $this->db->where("DATE_FORMAT(a.tanggal, '%Y-%m-%d')=" , $date);
+            $db = $this->db->get("oprasional a");
+            $transaksiproject2 =  $db->result_array();
+            // $transaksiproject2 = $this->db->query("select * from oprasional a where DATE_FORMAT(a.tanggal, '%Y-%m-%d') ='$date'")->result_array();
+            foreach ($transaksiproject2 as $key => $value) {
+            
+                # code...
+                $row = array();
+                # code...
+                $row[]=$value["tanggal"];
+                $row[]= date("M",strtotime($value["tanggal"]));
+                $row[]=$value["kode_project"];
+                $row[]=$value["nama_akun"];
+                $row[]=$value["kode_akun"];
+                $row[]=$value["nama_akun"];
+                $row[]=$value["kategori"];
+                $row[]=$value["witelhoName"];
+                $row[]=$value["stoName"];
+                $row[]=$value["regional_id"];
+                $row[]=$value["pekerjaanName"];
+                $row[]=$value["keterangan"];
+                $row[]=$value["debit"];
+                $row[]=$value["kredit"];
+                $row[]=$value["diterimaoleh"];
+                $row[]=$value["dikirimoleh"];
+                $row[]=$value["mandor"];
+                $result[] = $row;
+            }
+            
+            $date = date ("Y-m-d", strtotime("+1 day", strtotime($date)));
+        }
+        
+        echo json_encode($result); 
+    }
+
 }
 ?>
--- application/views/reportkeuangan/view.php
+++ application/views/reportkeuangan/view.php
@@ -1,7 +1,9 @@
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.14.3/xlsx.full.min.js"></script>
 <div class="col-12">
     <div class="card">
         <div class="card-body">
-        <form method="get">
+        <form method="get" id="form-search">
             <div class="row">
                 <div class="col-5"><input type="date" class="form-control" value="<?=$this->input->get("mulai")?>" name="mulai" /></div>
                 <div class="col-5"><input type="date" class="form-control"  value="<?=$this->input->get("selesai")?>"  name="selesai" /></div>
@@ -34,7 +36,10 @@
                 <p class="h6"><?=rupiah($nilaikeluar->nilai)?></p>
                 <span class="description-text">Pengeluaran Project</span>
             </div>
+            
+            <button onclick="excel()"  id="exportbtn" class="btn btn-success"><i class="fa fa-print"></i> Export</button>
         </div>
+
         </div>
 
         </div>
@@ -105,3 +110,75 @@
         </div>
     </div>
 </div>
+<script>
+function excel(){
+          var m =  $('#form-search').serializeArray();
+          var $thisbtn = $("#exportbtn");
+          $thisbtn.prop("disabled", true);
+          $($thisbtn).html(
+              `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span> Loading...`
+          );
+          $.ajax({
+              url: "<?=base_url("reportkeuangan/export?mulai=")?>"+m[0].value+"&selesai="+m[1].value,
+              type: "post",
+              dataType: "json",
+              data: m,
+              success: function(data) {
+
+                let Heading = [
+                    ['Tanggal', 'Bulan', 'Kode Project', 'Nama Akun', 'Kode Akun', 'Akun', 'kategori', 'witel', 'sto', 'regional', 'pekerjaan', 'Keterangan', 'debit', 'kredit', 'diterima oleh', 'dikirim oleh', 'mandor']
+                ];
+                var myFile = "Report_PENYELESAIAN_" + Date.now() + ".xlsx";
+                var myWorkSheet = XLSX.utils.json_to_sheet(data);
+                XLSX.utils.sheet_add_aoa(myWorkSheet, Heading, {
+                    origin: 0
+                });
+                myWorkSheet['!cols'] = [{
+                    width: 20
+                }, {
+                    width: 10
+                }, {
+                    width: 30
+                }, {
+                    width: 30
+                }, {
+                    width: 30
+                }, {
+                    width: 50
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 40
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }, {
+                    width: 20
+                }];
+                // var merges = myWorkSheet['!merges'] = [{ s: 'A1', e: 'D1' }];
+                var myWorkBook = XLSX.utils.book_new();
+                XLSX.utils.book_append_sheet(myWorkBook, myWorkSheet, "myWorkSheet");
+                XLSX.writeFile(myWorkBook, myFile);
+                console.log(data)
+                $thisbtn.prop("disabled", false);
+                $($thisbtn).html(
+                    `<i class="fa fa-print"></i> Export`
+                );
+              }
+            });
+        }
+
+</script>
